c518674f2043fa994f7e4a0a716ae54f23ed6c9d
Jason Kulasingam <jason@Jasons-MacBook-Pro.local>
2017-09-30 06:43:23 -0500

    trivia

---
+++ assets/javascript/app.js
@@ -0,0 +1,112 @@
+
+var questions = [{
+    question: "What is the fear of clowns called?",
+    choices: ["Clownaphobia", "IT Syndrome", "Coulrophobia", "Barnumalia"],
+    correctAnswer: 2
+}, {
+    question: "Guinness beer was first brewed in what country?",
+    choices: ["England", "Scotland", "Wales", "Ireland"],
+    correctAnswer: 3
+}, {
+    question: "What year did Nintendo first release its game console in North America?",
+    choices: ["1985", "1979", "1993", "1991"],
+    correctAnswer: 0
+}, {
+    question: "When adjusted for inflation, what is the highest grossing film of all time?",
+    choices: ["Titanic", "Gone With The Wind", "Avatar", "Star Wars"],
+    correctAnswer: 1
+}, {
+    question: "What popular soda was originally developed as a mixer for whiskey?",
+    choices: ["Coca Cola", "Ginger Ale", "Club Soda", "Mountain Dew"],
+    correctAnswer: 3
+}];
+
+var currentQuestion = 0;
+var correctAnswers = 0;
+var quizOver = false;
+
+$(document).ready(function () {
+
+    // Display the first question
+
+    displayCurrentQuestion();
+    $(this).find(".quizMessage").hide();
+
+    // On clicking next, display the next question
+    $(this).find(".nextButton").on("click", function () {
+        if (!quizOver) {
+
+            value = $("input[type='radio']:checked").val();
+
+            if (value == undefined) {
+                $(document).find(".quizMessage").text("Please select an answer");
+                $(document).find(".quizMessage").show();
+            } else {
+                // TODO: Remove any message -> not sure if this is efficient to call this each time....
+                $(document).find(".quizMessage").hide();
+
+                if (value == questions[currentQuestion].correctAnswer) {
+                    correctAnswers++;
+                }
+
+                currentQuestion++; // Since we have already displayed the first question on DOM ready
+                if (currentQuestion < questions.length) {
+                    displayCurrentQuestion();
+                } else {
+                    displayScore();
+                    $(document).find(".nextButton").toggle();
+                    $(document).find(".playAgainButton").toggle();
+                    // Change the text in the next button to ask if user wants to play again
+                    $(document).find(".nextButton").text("Play Again?");
+                    quizOver = true;
+                }
+            }
+        } else { // quiz is over and clicked the next button (which now displays 'Play Again?'
+            quizOver = false;
+            $(document).find(".nextButton").text("Next Question");
+            resetQuiz();
+            displayCurrentQuestion();
+            hideScore();
+        }
+    });
+
+});
+
+// This displays the current question AND the choices
+function displayCurrentQuestion() {
+
+    console.log("In display current Question");
+
+    var question = questions[currentQuestion].question;
+    //var questionClass = $(document).find(".quizContainer > .question");
+    var questionClass = $(document).find(".holder > .question");
+    var choiceList = $(document).find(".quizContainer > .choiceList");
+    var numChoices = questions[currentQuestion].choices.length;
+
+    // Set the questionClass text to the current question
+    $(questionClass).text(question);
+
+    // Remove all current <li> elements (if any)
+    $(choiceList).find("li").remove();
+
+    var choice;
+    for (i = 0; i < numChoices; i++) {
+        choice = questions[currentQuestion].choices[i];
+        $('<li><input type="radio" value=' + i + ' name="dynradio" />' + choice + '</li>').appendTo(choiceList);
+    }
+}
+
+function resetQuiz() {
+    currentQuestion = 0;
+    correctAnswers = 0;
+    hideScore();
+}
+
+function displayScore() {
+    $(document).find(".quizContainer > .result").text("You scored: " + correctAnswers + " out of: " + questions.length);
+    $(document).find(".quizContainer > .result").show();
+}
+
+function hideScore() {
+    $(document).find(".result").hide();
+}
